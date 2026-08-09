57caf43194e7041e7925a64ad8ac57d8473bdfe8
tison <wander4096@gmail.com>
2026-08-09 14:57:25 +0800

    bench: cover keyed once primitives

--- mea/benches/primitives/main.rs
+++ mea/benches/primitives/main.rs
@@ -12,7 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod once_map;
 mod oneshot;
+mod singleflight;
+mod support;
 
 fn main() {
     divan::main();
---
+++ mea/benches/primitives/support.rs
@@ -0,0 +1,37 @@
+// Copyright 2024 tison <wander4096@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::future::Future;
+use std::pin::pin;
+use std::task::Context;
+use std::task::Poll;
+use std::task::Waker;
+
+pub(super) fn noop_context() -> Context<'static> {
+    Context::from_waker(Waker::noop())
+}
+
+pub(super) fn poll_ready<F: Future>(future: F, context: &mut Context<'_>) -> F::Output {
+    let mut future = pin!(future);
+    match future.as_mut().poll(context) {
+        Poll::Ready(output) => output,
+        Poll::Pending => panic!("benchmark future should be ready"),
+    }
+}
+
+// Move the input into the benchmark output so Divan drops it outside the timed section.
+#[inline]
+pub(super) fn defer_input_drop<I, O>(input: I, output: O) -> (I, O) {
+    (input, output)
+}
