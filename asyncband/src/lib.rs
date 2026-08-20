fe6d767512e0b73b050bbc475c358cae0f2577c7
tison <wander4096@gmail.com>
2026-08-20 23:41:20 +0800

    docs: inline the incubation disclaimer

--- asyncband/src/lib.rs
+++ asyncband/src/lib.rs
@@ -25,8 +25,14 @@
 //! efficient synchronization tools that work with any async runtime.
 //!
 //! > Apache Asyncband is an effort undergoing incubation at the Apache Software Foundation (ASF),
-//! > sponsored by the Apache Incubator PMC. Please read the
-//! > [DISCLAIMER](https://github.com/apache/asyncband/blob/main/DISCLAIMER).
+//! > sponsored by the Apache Incubator PMC.
+//! >
+//! > Incubation is required of all newly accepted projects until a further review indicates that
+//! > the infrastructure, communications, and decision making process have stabilized in a manner
+//! > consistent with other successful ASF projects.
+//! >
+//! > While incubation status is not necessarily a reflection of the completeness or stability of
+//! > the code, it does indicate that the project has yet to be fully endorsed by the ASF.
 //!
 //! # Migrating from MEA
 //!
